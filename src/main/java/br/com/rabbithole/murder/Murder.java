63e0cba60bab23e7ce373afba9184cc90acb5b88
FelipeSz19 <feiperos.simao@gmail.com>
2023-03-12 19:55:45 -0300

    registers

--- src/main/java/br/com/rabbithole/murder/Murder.java
+++ src/main/java/br/com/rabbithole/murder/Murder.java
@@ -1,5 +1,6 @@
 package br.com.rabbithole.murder;
 
+import org.bukkit.event.HandlerList;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Murder extends JavaPlugin {
@@ -7,11 +8,21 @@ public final class Murder extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        registers();
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        HandlerList.unregisterAll(this);
     }
+
+    void registers() {
+        commands();
+        events();
+    }
+
+    void commands() {}
+
+    void events() {}
 }
